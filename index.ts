3e18d92517fa9099efe31af805f8f85d7bdd0279
MisbahDayo <dayomuhammad892@gmail.com>
2024-03-28 00:28:47 +0500

    created number guessing the game

---
+++ index.ts
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer'
+
+//1)computer will generate a rendom number 
+//2) user input for guessing number 
+//3) compare user with computer generated number  
+
+const randomnumber= 13;
+const answer=await inquirer.prompt([
+
+    {
+        name: "userGuessNumber",
+          type: "number",
+          message: "please guess a number",}
+    ]);
+if(answer.userGuessNumber===randomnumber)
+
+    {console.log("Coguratulation! you guess right number")} 
+
+else { console.log("you guess worng number")};
